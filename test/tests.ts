29e25d2c110a3a3b3193724652bce18b100f652a
wanderer06 <georgica_fara_de_frica@yahoo.com>
2016-04-18 21:24:24 +0100

    Start matcher testing

--- test/tests.ts
+++ test/tests.ts
@@ -1,10 +1,34 @@
-import {expect, assert} from 'chai';
+import {expect} from 'chai';
+import {Matcher} from '../source/matcher.class';
 
-describe('Useless tests', () => {
-  it('should just pass these', () => {
-    // useless test
-    let foo: string = 'bar';
-    assert.typeOf(foo, 'string');
-    expect(5).to.equal(5);
+const spells = {
+  'lina': [
+    'Bram Gush',
+    'Burst Flare',
+    'Demonic Crystal',
+    'Digger Volt',
+    'Dragon Slave',
+    'Fireball',
+    'Giga Slave',
+    'Laguna Blade',
+    'Ra Tilt',
+    'Van Rehl'
+  ]
+};
+
+describe('Matchers', () => {
+  describe('matchStart', () => {
+    const matcher = Matcher.matchStart;
+
+    it('Matches start of string', () => {
+      let search = 'D';
+      // let match = spells.lina.filter()
+      expect(matcher('D', spells.lina[3])).to.be.true;
+    });
   });
+
+  describe('matchAnywhere', () => { });
+  describe('matchAnywhereSparse', () => { });
+  describe('matchExactly', () => { });
+
 });
